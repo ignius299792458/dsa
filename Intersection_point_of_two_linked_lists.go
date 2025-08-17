71476c61372c29dea2526d39369c4182df88835b
ignius299792458 <bogati.mahesh.299.792.458@gmail.com>
2025-08-17 21:48:02 +0545

    CH: Intersection_point_of_two_linked_lists

--- Intersection_point_of_two_linked_lists.go
+++ Intersection_point_of_two_linked_lists.go
@@ -1,35 +1,67 @@
 package main
 
+import "fmt"
+
 // create singly linked list
 type Node struct {
 	value any
 	next  *Node
 }
 
-var head Node
-
-type SinglyLLInterface interface {
-	AddToLink(value any)
+type SLL struct {
+	head Node
 }
 
-func (n *Node) AddToLink(value any) {
+type SLLInterface interface {
+	display()
+}
+
+func add(sll *SLL, value any) {
 
 	// create head
-	if head.value == nil && head.next == nil {
-		head.value = value
-		head.next = nil
+	if sll.head.next == nil {
+		sll.head.value = value
+		sll.head.next = nil
 		return
 	}
 
-	prevNode := head
-	var newNode Node
-	prevNode.next = &newNode
-	for prevNode.next != nil {
-
+	currentNode := sll.head
+	var newNode = Node{value: value, next: nil}
+	for currentNode.next != nil {
+		currentNode = *currentNode.next
+		if currentNode.next != nil {
+			currentNode.next = &newNode
+			currentNode = newNode
+		}
 	}
-
 }
 
-// create 2 sll
+func NewSLL(values ...any) SLL {
+	var sll SLL
+	for _, value := range values {
+		fmt.Printf("Appending %v->", value)
+		add(&sll, value)
+		sll.display()
+	}
+	return sll
+}
 
-// make intersection of them
+func (sll *SLL) display() {
+	fmt.Println("\nSLL : ", sll)
+	currentNode := sll.head
+	for currentNode.next != nil {
+		fmt.Printf("%v->", currentNode.value)
+		currentNode = *currentNode.next
+	}
+}
+
+func main() {
+	// create 2 sll
+	// 1. sll
+	sll1 := NewSLL(1, 2, 3, 6, 5, 6)
+	sll1.display()
+	// sll2 := NewSLL(30, 31, 32, 35, 37, 43)
+	// sll2.display()
+
+	// make intersection of them
+}
